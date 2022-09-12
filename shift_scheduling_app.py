d495ba5b670a7016d2416a1eb6afa2f4b7b1b7e2
occ-nisiyama <113075510+occ-nisiyama@users.noreply.github.com>
2022-09-12 11:48:13 +0900

    Update shift_scheduling_app.py

--- shift_scheduling_app.py
+++ shift_scheduling_app.py
@@ -23,3 +23,26 @@ if solver_type == "Constrained Quadratic Model":
     use_cqm_solver = True
 else:
     use_cqm_solver = False
+
+    
+import pandas as pd
+list1=[[1,2,3], [4,5,6], [7,8,9]]
+index1 = ["Row1", "Row2", "Row3"]
+columns1 =["Col1", "Col2", "Col3"]
+df =pd.DataFrame(data=list1, index=index1, columns=columns1)
+
+def rain_condition(v):
+    if v < 1.75:
+        return "Dry"
+    elif v < 2.75:
+        return "Rain"
+    return "Heavy Rain"
+
+def make_pretty(styler):
+    styler.set_caption("Weather Conditions")
+    styler.format(rain_condition)
+    #styler.format_index(lambda v: v.strftime("%A"))
+    styler.background_gradient(axis=None, vmin=1, vmax=9, cmap="Pastel1")
+    return styler
+
+st.dataframe(df.style.pipe(make_pretty))
