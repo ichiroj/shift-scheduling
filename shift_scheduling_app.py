05cb5b632e94da3120c72365bac528ad01ce91a6
occ-nisiyama <113075510+occ-nisiyama@users.noreply.github.com>
2022-09-13 13:57:33 +0900

    Update shift_scheduling_app.py

--- shift_scheduling_app.py
+++ shift_scheduling_app.py
@@ -4,15 +4,14 @@ import sys
 import streamlit as st
 from typing import Optional
 
-from shift_scheduling import (Variables, build_cqm, call_solver, make_df)
-
-
+from shift_scheduling import (Variables, build_cqm, call_solver, make_df, dow_chr, wd_chr)
 
 def solve_shift_scheduling(opts: dict):
     vars = Variables(opts)
     cqm = build_cqm(opts, vars)
     best_feasible = call_solver(cqm, opts)
     df = make_df(best_feasible, opts, vars)
+    df = df.style.applymap(lambda v: 'background-color: red;' if v == wd_chr[0] else None)
     st.dataframe(df)
     
 st.set_page_config(layout="wide")
@@ -33,55 +32,22 @@ else:
 run_type = st.sidebar.radio(label="Choose run type:",
                             options=["Random", "File upload"])
 
-"""
-import pandas as pd
-list1=[[1,2,3], [4,5,6], [7,8,9]]
-index1 = ["Row1", "Row2", "Row3"]
-columns1 =["Col1", "Col2", "Col3"]
-df =pd.DataFrame(data=list1, index=index1, columns=columns1)
-
-def rain_condition(v):
-    if v < 1.75:
-        return "Dry"
-    elif v < 2.75:
-        return "Rain"
-    return "Heavy Rain"
-
-def make_pretty(styler):
-    styler.set_caption("Weather Conditions")
-    styler.format(rain_condition)
-    #styler.format_index(lambda v: v.strftime("%A"))
-    styler.background_gradient(axis=None, vmin=1, vmax=9, cmap="Pastel1")
-    return styler
-
-st.dataframe(df.style.pipe(make_pretty))
-"""
-
 run_button = st.sidebar.button("Run")
 
 workers_range = st.sidebar.slider("人数", min_value=5, max_value=26, value=20)
 days_range = st.sidebar.slider("日数", min_value=28, max_value=31, value=30)
-fst_dow = st.sidebar.selectbox("曜日", ['月','火','水','木','金','土','日'])
+fst_dow = st.sidebar.selectbox("曜日", dow_chr)
 
-#col1, col2 = st.columns([1, 2])
-#with col1:
-#    pass
-    #st.dataframe(df.style.pipe(make_pretty))
-#with col2:
 if run_button:
 
     options = {
-        'use_cqm_solver': False,
         'time_limit': 120,
-        'num_workers': 20,
-        'num_days': 29,
-        'fst_dow': 5,
     }
+
+    options['use_cqm_solver'] = use_cqm_solver
 
     options['num_workers'] = workers_range
     options['num_days'] = days_range
     options['fst_dow'] = int(fst_dow)
 
     solve_shift_scheduling(options)
-
-
