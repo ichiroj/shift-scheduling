c88924e19278935d5580b923fe49ef60620b697e
occ-nisiyama <113075510+occ-nisiyama@users.noreply.github.com>
2022-09-16 17:52:21 +0900

    Update shift_scheduling_app.py

--- shift_scheduling_app.py
+++ shift_scheduling_app.py
@@ -17,30 +17,45 @@ def solve_shift_scheduling(opts: dict):
     
 st.set_page_config(layout="wide")
 st.markdown(
-    "<h1 style='text-align: center;'>Shift Scheduling Demo</h1>",
+    "<h1 style='text-align: center;'>シフトスケジュール　デモ</h1>",
     unsafe_allow_html=True
 )
 
-solver_type = st.sidebar.radio(label="量子コンピュータを：",
-                               options=["使う (LeapHybridCQMSampler)",
-                                        "使わない (Python-MIP)"],
-                               index=1)
-if solver_type == "使う (LeapHybridCQMSampler)":
-    use_cqm_solver = True
-else:
-    use_cqm_solver = False
+with st.expander("【 条件の種類 】"):
+    st.markdown(
+        """
+        1. ３～６日連続勤務で１日休み（全員／個別）
+        2. 
+        """
+    )
 
-time_limit = st.sidebar.number_input(label="時間制限（秒）", value=20)
+with st.sidebar.expander("【 量子コンピュータ設定 】"):
+    solver_type = st.radio(label="量子コンピュータを：",
+                                options=["使う (LeapHybridCQMSampler)",
+                                            "使わない (Python-MIP)"],
+                                index=1)
+    if solver_type == "使う (LeapHybridCQMSampler)":
+        use_cqm_solver = True
+    else:
+        use_cqm_solver = False
 
-workers_range = st.sidebar.slider("人数：", min_value=5, max_value=26, value=20)
-days_range = st.sidebar.slider("日数：", min_value=28, max_value=31, value=30)
-fst_dow_chr = st.sidebar.selectbox("開始曜日：", dow_chr)
+    time_limit = st.number_input(label="時間制限（秒）：", value=20)
 
-chk_consecutive = st.sidebar.checkbox("？日連続勤務をしたら１日休みを割り当てる", value=True)
-if chk_consecutive:
-    consecutive_days = st.sidebar.slider("連続勤務日数：", min_value=3, max_value=6, value=5)
-else:
-    consecutive_days = 0
+with st.sidebar.expander("【 基本設定 】"):
+    workers_range = st.slider("人数：", min_value=5, max_value=26, value=20)
+    workers_list = [chr(ord('A') + w) for w in range(workers_range)]
+    days_range = st.slider("日数：", min_value=28, max_value=31, value=30)
+    fst_dow_chr = st.selectbox("開始曜日：", dow_chr)
+
+with st.sidebar.expander("【 ３～６日連続勤務で１日休み 】"):
+    cond01_all_chk = st.checkbox("全員")
+    cond01_all_days = st.slider("連続勤務日数（全員）：", min_value=3, max_value=6, value=5)
+    st.markdown("<hr/>", unsafe_allow_html=True)
+    cond01_sel_chk = st.checkbox("個別")
+    cond01_sel_days = st.slider("連続勤務日数（個別）：", min_value=3, max_value=6, value=5)
+    cond01_sel_wrks = st.multiselect("対象者選択：",  workers_list)
+
+
 
 run_button = st.sidebar.button("Run")
 
@@ -57,7 +72,10 @@ if run_button:
     options['num_days'] = days_range
     options['fst_dow'] = dow_chr.index(fst_dow_chr)
 
-    options['chk_consecutive'] = chk_consecutive
-    options['consecutive_days'] = consecutive_days
+    options['cond01_all_chk'] = cond01_all_chk
+    options['cond01_all_days'] = cond01_all_days
+    options['cond01_sel_chk'] = cond01_sel_chk
+    options['cond01_sel_days'] = cond01_sel_days
+    options['cond01_sel_wrks'] = cond01_sel_wrks
 
     solve_shift_scheduling(options)
