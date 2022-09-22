65fe1e74e51010168fed675861a173322ce101e7
occ-nisiyama <113075510+occ-nisiyama@users.noreply.github.com>
2022-09-22 15:51:02 +0900

    Update shift_scheduling.py

--- shift_scheduling.py
+++ shift_scheduling.py
@@ -17,64 +17,85 @@ options = {
     'num_workers': 20,
     'num_days': 30,
     'fst_dow': 0,
+    'obj_sign': -1,
 
+    # 1. ３～６日連続勤務で１日休み（全員／個別）
     'cond01_all_chk': False,
     'cond01_all_days': 5,
     'cond01_sel_chk': False,
     'cond01_sel_days': 5,
     'cond01_sel_wrks': [],
 
+    # 2. 土日連休を月１～４回以上割当（全員／個別）
     'cond02_all_chk': False,
     'cond02_all_cnt': 1,
     'cond02_sel_chk': False,
     'cond02_sel_cnt': 1,
     'cond02_sel_wrks': [],
 
+    # 3. 土日を休みにする（全員／個別）
     'cond03_all_chk': False,
     'cond03_sel_chk': False,
     'cond03_sel_wrks': [],
 
+    # 4. 休みを月４～１０回割り当てる（全員／個別）
     'cond04_all_chk': False,
-    'cond04_all_cnt': 1,
+    'cond04_all_cnt': 8,
     'cond04_sel_chk': False,
-    'cond04_sel_cnt': 1,
+    'cond04_sel_cnt': 8,
     'cond04_sel_wrks': [],
 
+    # 5. 休→出→休の飛び石連休はなし
     'cond05_chk': False,
 
+    # 6. 休みを週に１～６回以上割当（全員／個別）
     'cond06_all_chk': False,
     'cond06_all_days': 2,
     'cond06_sel_chk': False,
     'cond06_sel_days': 2,
     'cond06_sel_wrks': [],
 
+    # 7. １日の出勤人数はＸ人以上（平日／土日）
     'cond07_wrk_chk': False,
-    'cond07_wrk_cnt': 1,
+    'cond07_wrk_cnt': 16,
     'cond07_hol_chk': False,
-    'cond07_hol_cnt': 1,
+    'cond07_hol_cnt': 16,
 
     # 8. 月の出勤日数を４～２４日以上（全員／個別）
-    'cond08_all_chk': True,
-    'cond08_all_days': 5,
+    'cond08_all_chk': False,
+    'cond08_all_days': 20,
     'cond08_sel_chk': False,
-    'cond08_sel_days': 5,
+    'cond08_sel_days': 20,
     'cond08_sel_wrks': [],
 
+    # 9. 週の出勤日数は１～６日以上（全員／個別）
     'cond09_all_chk': False,
     'cond09_all_days': 5,
     'cond09_sel_chk': False,
     'cond09_sel_days': 5,
     'cond09_sel_wrks': [],
 
-    'cond10_chk': True,
-    'cond10_wrks_A': ['A','B'],
+    # 10. 一緒に勤務させる
+    'cond10_chk': False,
+    'cond10_wrks_A': [],
     'cond10_wrks_B': [],
     'cond10_wrks_C': [],
 
+    # 11. 一緒に勤務させない
     'cond11_chk': False,
     'cond11_wrks_A': [],
     'cond11_wrks_B': [],
     'cond11_wrks_C': [],
+
+    # 12. 一緒に勤務させない
+    'cond12_chk': False,
+    'cond12_days': [],
+    'cond12_wrks': [],
+
+    # 13. 一緒に勤務させない
+    'cond13_chk': False,
+    'cond13_dows': [],
+    'cond13_wrks': [],
 }
 
 class Variables:
@@ -85,9 +106,9 @@ class Variables:
         self.wd = {(w, d): Binary(f'worker_{w}_day_{d}') for w in range(num_workers) for d in range(num_days)}
         if opts['cond02_all_chk'] or opts['cond02_sel_chk']:
             self.wwe = {(w, we): Binary(f'worker_{w}_weekend_{we}') for w in range(num_workers) for we in range(4)}
-        if opts['cond11_chk']:
-            self.dww = {(d, w1, w2): Binary(f'day_{d}_worker1_{w1}_weekend2_{w2}')
-                for d in range(num_days) for w1 in range(num_workers) for w2 in range(num_workers)}
+        #if opts['cond11_chk']:
+        #    self.dww = {(d, w1, w2): Binary(f'day_{d}_worker1_{w1}_weekend2_{w2}')
+        #        for d in range(num_days) for w1 in range(num_workers) for w2 in range(num_workers)}
 
 def add_constraints(cqm: ConstrainedQuadraticModel, opts: dict, vars: Variables):
     num_workers = opts['num_workers']
@@ -187,14 +208,28 @@ def add_constraints(cqm: ConstrainedQuadraticModel, opts: dict, vars: Variables)
             for grp_chr in [opts['cond11_wrks_A'], opts['cond11_wrks_B'], opts['cond11_wrks_C']]:
                 grp_num = list(map(lambda x: wrk_chr.index(x), grp_chr))
                 for cmb in itertools.combinations(grp_num, 2):
-                    cqm.add_constraint(2 * vars.dww[d,cmb[0],cmb[1]] - vars.wd[cmb[0],d] - (1 - vars.wd[cmb[1],d]) <= 0)  
-                    cqm.add_constraint(2 * (1 - vars.dww[d,cmb[0],cmb[1]]) - (1 - vars.wd[cmb[0],d]) - vars.wd[cmb[1],d] <= 0)  
+                    #cqm.add_constraint(2 * vars.dww[d,cmb[0],cmb[1]] - vars.wd[cmb[0],d] - (1 - vars.wd[cmb[1],d]) <= 0)  
+                    #cqm.add_constraint(2 * (1 - vars.dww[d,cmb[0],cmb[1]]) - (1 - vars.wd[cmb[0],d]) - vars.wd[cmb[1],d] <= 0) 
+                    cqm.add_constraint(vars.wd[cmb[0],d] + vars.wd[cmb[1],d] - 1 <= 0) 
+
+    # 12. 特定の日を休みにする（個別）
+    if opts['cond12_chk']:
+        for w in list(map(lambda x: wrk_chr.index(x), opts['cond12_wrks'])):
+            for d in opts['cond12_days']:
+                cqm.add_constraint(vars.wd[w,d-1] == 0) 
+
+    # 13. 特定の曜日を休みにする（個別）
+    if opts['cond13_chk']:
+        for w in list(map(lambda x: wrk_chr.index(x), opts['cond13_wrks'])):
+            for dow in list(map(lambda x: dow_chr.index(x), opts['cond13_dows'])):
+                for d in [x for x in range(num_days) if ((x + fst_dow) % 7) == dow]:
+                    cqm.add_constraint(vars.wd[w,d] == 0) 
 
 def define_objective(cqm: ConstrainedQuadraticModel, opts: dict, vars: Variables):
     num_workers = opts['num_workers']
     num_days = opts['num_days']
-
-    cqm.set_objective(quicksum(+vars.wd[w, d] for w in range(num_workers) for d in range(num_days)))
+    obj_sign = opts['obj_sign']
+    cqm.set_objective(quicksum(obj_sign * vars.wd[w, d] for w in range(num_workers) for d in range(num_days)))
 
 def build_cqm(opts: dict, vars: Variables) -> ConstrainedQuadraticModel:
     cqm = ConstrainedQuadraticModel()
